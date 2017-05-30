3aea489bb55e0dcc05f61a3bfc0ffa2438422aa2
dtext <textores.danny@gmail.com>
2017-05-30 16:23:05 +0200

    Add initial version of script without having looked at the data

---
+++ tconv.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+import sys
+import csv
+
+# table headings
+head = """
+<table>
+  <thead>
+    <tr>
+      <td>Tag</td>
+      <td colspan="5">Montag</td>
+      <td colspan="5">Dienstag</td>
+      <td colspan="5">Mittwoch</td>
+      <td colspan="5">Donnerstag</td>
+      <td colspan="5">Freitag</td>
+    </tr>
+    <tr>
+      <td>Uhrzeit</td>
+      <td>8</td><td>10</td><td>12</td><td>14</td><td>16</td>
+      <td>8</td><td>10</td><td>12</td><td>14</td><td>16</td>
+      <td>8</td><td>10</td><td>12</td><td>14</td><td>16</td>
+      <td>8</td><td>10</td><td>12</td><td>14</td><td>16</td>
+      <td>8</td><td>10</td><td>12</td><td>14</td><td>16</td>
+    </tr>
+  </thead>
+  <tbody>
+"""
+
+# template for new person
+template = """
+    <tr>
+      <td>{}</td>
+      <td>{}</td><td>{}</td><td>{}</td><td>{}</td><td>{}</td>
+      <td>{}</td><td>{}</td><td>{}</td><td>{}</td><td>{}</td>
+      <td>{}</td><td>{}</td><td>{}</td><td>{}</td><td>{}</td>
+      <td>{}</td><td>{}</td><td>{}</td><td>{}</td><td>{}</td>
+      <td>{}</td><td>{}</td><td>{}</td><td>{}</td><td>{}</td>
+    </tr>
+"""
+
+# closing tags at the end of the table
+tail = """
+  </tbody>
+</table>
+"""
+
+# map doodle's values to our values
+value_mapping = {
+    "OK": " &#10004; ",
+    "ifneedbe": "(&#10004;)",
+    "": "          ",
+}
+
+
+def mapping(value: str) -> str:
+    return value_mapping.get(value, default=value)
+
+# process csv
+output = head
+reader = csv.reader(sys.argv[0])
+for row in reader:
+    output += template.format(*map(mapping, row))
+output += tail
+print(output)
