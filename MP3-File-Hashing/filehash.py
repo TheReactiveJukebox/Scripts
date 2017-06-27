1bef3d245a30543b4d9ed0c285995c60e3402cd3
Florian Lüdiger <florian.luediger@googlemail.com>
2017-06-27 20:54:16 +0200

    [FileHashing] Add script to hash filenames of the mp3 files and reorganize them into subfolders.

---
+++ MP3-File-Hashing/filehash.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import os
+import hashlib
+
+path = "."
+
+
+def rename():
+    for filename in os.listdir(path):
+        m = hashlib.sha256()
+        m.update(str(filename).encode('utf-8'))
+        os.rename(path + "/" + filename, path + "/" + m.hexdigest() + ".mp3")
+
+
+def reorganize():
+    for filename in os.listdir(path):
+        first = filename[0]
+        second = filename[1]
+        if not os.path.exists(path + "/" + first + "/" + second):
+            os.makedirs(path + "/" + first + "/" + second)
+        os.rename(path + "/" + filename, path + "/" + first + "/" + second + "/" + filename[2:])
+
+
+rename()
+reorganize()
