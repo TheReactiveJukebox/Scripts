e2edd67c8cdbbc1f3f1cfbf5b4dd9f8b331c34f4
Florian Lüdiger <florian.luediger@googlemail.com>
2017-07-25 13:17:07 +0200

    [BPM] Fetch track-ids from Spotify

--- BpmAnalyzer/BpmAnalyzer.py
+++ BpmAnalyzer/BpmAnalyzer.py
@@ -2,6 +2,7 @@ import csv
 import logging
 
 import constants
+import requests
 from oauthtool import implicit_flow
 
 
@@ -24,6 +25,8 @@ csv_out = csv.writer(csv_outfile, delimiter=',')
 csv_infile = open('data.csv', "r", encoding="utf-8")
 csv_in = csv.reader(csv_infile, delimiter=',')
 
+not_found_counter = 0
+
 for rownum, row in enumerate(csv_in):
     if rownum > 50:
         break
@@ -32,7 +35,22 @@ for rownum, row in enumerate(csv_in):
                           'albummbid', 'playcount', 'listeners', 'albumcover', 'genres', 'artistrating', 'trackrating', 'bpm'))
         continue  # next row
 
-    bpm = 0
+    search_request_param = {"access_token": access_token,
+                            "q": row[0] + " " + row[1],
+                            "type": "track",
+                            "limit": 1}
+    search_request = requests.get(constants.spotifyBaseUrl + "/search",
+                                     params=search_request_param)
+    search_data = search_request.json()
+
+    if len(search_data["tracks"]["items"]) == 0:
+        not_found_counter += 1
+        bpm = 0
+    else:
+        track_id = search_data["tracks"]["items"][0]["id"]
+        bpm = 0
 
     csv_out.writerow((row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7],
                       row[8], row[9], row[10], row[11], row[12], row[13], row[14], bpm))
+
+print(not_found_counter)
