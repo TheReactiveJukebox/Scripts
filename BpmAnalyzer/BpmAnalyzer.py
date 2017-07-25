5575b67596226ac0802c277a9ee9c85aa3d77f99
Florian Lüdiger <florian.luediger@googlemail.com>
2017-07-25 12:45:06 +0200

    [BPM] Add Spotify authentication

---
+++ BpmAnalyzer/BpmAnalyzer.py
@@ -0,0 +1,20 @@
+import logging
+
+import constants
+from oauthtool import implicit_flow
+
+
+def _authorize():
+    # Start OAuth2 implicit flow
+    auth_response = implicit_flow(constants.authorizeUrl, constants.clientId)
+
+    # Check if authorization was successful
+    if "error" in auth_response and auth_response["error"] is not None:
+        logging.error("Authentication failed. Error message: {0}".format(auth_response["error_description"]))
+        return False
+
+    return auth_response["access_token"]
+
+access_token = _authorize()
+
+print(access_token)
---
+++ BpmAnalyzer/constants.py
@@ -0,0 +1,4 @@
+authorizeUrl = "https://accounts.spotify.com/authorize"
+clientId = "your_client_id_here"
+spotifyBaseUrl = "https://api.spotify.com/v1"
+spotifyUser = "your_user_here"
