56e12794de5fcef6b8602aba505bcde94a5167d2
Florian Lüdiger <florian.luediger@googlemail.com>
2017-07-25 12:59:43 +0200

    [BPM] Add CSV handling

--- BpmAnalyzer/BpmAnalyzer.py
+++ BpmAnalyzer/BpmAnalyzer.py
@@ -1,3 +1,4 @@
+import csv
 import logging
 
 import constants
@@ -17,4 +18,21 @@ def _authorize():
 
 access_token = _authorize()
 
-print(access_token)
+csv_outfile = open("data_out.csv", "w", encoding="utf-8", newline="")
+csv_out = csv.writer(csv_outfile, delimiter=',')
+
+csv_infile = open('data.csv', "r", encoding="utf-8")
+csv_in = csv.reader(csv_infile, delimiter=',')
+
+for rownum, row in enumerate(csv_in):
+    if rownum > 50:
+        break
+    if (rownum == 0):  # header
+        csv_out.writerow(('title', 'artist', 'album', 'songHash', 'length', 'published', 'trackmbid', 'artistmbid',
+                          'albummbid', 'playcount', 'listeners', 'albumcover', 'genres', 'artistrating', 'trackrating', 'bpm'))
+        continue  # next row
+
+    bpm = 0
+
+    csv_out.writerow((row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7],
+                      row[8], row[9], row[10], row[11], row[12], row[13], row[14], bpm))
