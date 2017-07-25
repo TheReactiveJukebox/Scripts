c2240c4833e506ce185b97da6193e35f8d734cf8
Florian Lüdiger <florian.luediger@googlemail.com>
2017-07-25 16:42:50 +0200

    [SpotifyFeatures] Add music features to CSV file

--- BpmAnalyzer/BpmAnalyzer.py
+++ BpmAnalyzer/BpmAnalyzer.py
@@ -17,6 +17,7 @@ def _authorize():
 
     return auth_response["access_token"]
 
+
 access_token = _authorize()
 
 csv_outfile = open("data_out.csv", "w", encoding="utf-8", newline="")
@@ -28,11 +29,11 @@ csv_in = csv.reader(csv_infile, delimiter=',')
 not_found_counter = 0
 
 for rownum, row in enumerate(csv_in):
-    if rownum > 50:
-        break
-    if (rownum == 0):  # header
+    if rownum == 0:  # header
         csv_out.writerow(('title', 'artist', 'album', 'songHash', 'length', 'published', 'trackmbid', 'artistmbid',
-                          'albummbid', 'playcount', 'listeners', 'albumcover', 'genres', 'artistrating', 'trackrating', 'bpm'))
+                          'albummbid', 'playcount', 'listeners', 'albumcover', 'genres', 'artistrating', 'trackrating',
+                          'bpm', 'danceability', 'energy', 'loudness', 'speechiness', 'acousticness',
+                          'instrumentalness', 'liveness'))
         continue  # next row
 
     search_request_param = {"access_token": access_token,
@@ -40,17 +41,24 @@ for rownum, row in enumerate(csv_in):
                             "type": "track",
                             "limit": 1}
     search_request = requests.get(constants.spotifyBaseUrl + "/search",
-                                     params=search_request_param)
+                                  params=search_request_param)
     search_data = search_request.json()
 
     if len(search_data["tracks"]["items"]) == 0:
         not_found_counter += 1
-        bpm = 0
+        csv_out.writerow((row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7],
+                          row[8], row[9], row[10], row[11], row[12], row[13], row[14], 0, 0, 0, 0, 0, 0, 0, 0))
     else:
         track_id = search_data["tracks"]["items"][0]["id"]
-        bpm = 0
+        feature_request_param = {"access_token": access_token}
+        feature_request = requests.get(constants.spotifyBaseUrl + "/audio-features/" + track_id,
+                                       params=feature_request_param)
+        feature_data = feature_request.json()
+        csv_out.writerow((row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7],
+                          row[8], row[9], row[10], row[11], row[12], row[13], row[14],
+                          feature_data["tempo"], feature_data["danceability"], feature_data["energy"],
+                          feature_data["loudness"], feature_data["speechiness"], feature_data["acousticness"],
+                          feature_data["instrumentalness"], feature_data["liveness"]))
 
-    csv_out.writerow((row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7],
-                      row[8], row[9], row[10], row[11], row[12], row[13], row[14], bpm))
 
 print(not_found_counter)
