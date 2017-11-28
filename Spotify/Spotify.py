5c15bf202a1ddf271e5801634e7d28fdf74d8684
Figch <interfisch@gmx.net>
2017-11-28 13:27:09 +0100

    [Spotify] add fix for using existing spotifydata. only temporary

--- Spotify/Spotify.py
+++ Spotify/Spotify.py
@@ -9,8 +9,8 @@ from oauthtool import implicit_flow
 
 import spotifySettings
 
-range_from = 20001
-range_to = 24000
+range_from = 0
+range_to = 100000000000
 
 def _authorize():
     # Start OAuth2 implicit flow
@@ -24,7 +24,7 @@ def _authorize():
     return auth_response["access_token"]
 
 
-delete_not_found = True
+delete_not_found = False
 
 if delete_not_found:
     path = sys.argv[2]
@@ -35,10 +35,15 @@ access_token = "123"
 csv_outfile = open("spotifydata.csv" + "_" + str(range_from) + "to" + str(range_to), "w", encoding="utf-8", newline="")
 csv_out = csv.writer(csv_outfile, delimiter=',')
 
-infile = sys.argv[1]
+infile = sys.argv[1] #lastfmdata.csv
 
-csv_infile = open(infile, "r", encoding="utf-8")
+csv_infile = open(infile, "r", encoding="utf-8") #lastfmdata.csv
 csv_in = csv.reader(csv_infile, delimiter=',')
+
+csvspotifyumlautbug_infile = open("spotifydata.csv_umlautbug", "r", encoding="utf-8") #umlautbugfix
+csvspotifyumlautbug_in = csv.reader(csvspotifyumlautbug_infile, delimiter=',') #umlautbugfix
+existingspotifydata=[ x for x in csvspotifyumlautbug_in] #umlautbugfix  lines to array
+existingspotifydata=np.array(existingspotifydata) #umlautbugfix
 
 not_found_counter = 0
 
@@ -51,6 +56,28 @@ for rownum, row in enumerate(csv_in):
         continue  # next row
     elif rownum < range_from or rownum > range_to:
         continue
+
+
+    ##  UMLAUTBUGFIX  skip songs which are already in existing spotifydata.csv and copy that line to new file
+    existingdata_indexfound=-1  #umlautbugfix
+    for edi,ed in enumerate(existingspotifydata[:,0:3]):  #umlautbugfix
+        exisintdatalinematch= (ed==row[0:3])  #umlautbugfix
+        if (np.all(exisintdatalinematch): #current index is line with this data  #umlautbugfix
+            existingdata_indexfound=edi  #umlautbugfix
+
+    efd=existingspotifydata #shorten vairabled  #umlautbugfix
+    efdi=existingdata_indexfound #shorten variabled  #umlautbugfix
+
+    if existingdata_indexfound>0: #line exists in spotifydata   #umlautbugfix
+        csv_out.writerow((row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7],
+                          row[8], row[9], row[10], row[11], row[12], row[13], row[14],
+                          efd[efdi][15], efd[efdi][16],efd[efdi][17],
+                          efd[efdi][18], efd[efdi][19], efd[efdi][20],
+                          efd[efdi][21], efd[efdi][22], efd[efdi][23],
+                          efd[efdi][24], efd[efdi][25]))  #umlautbugfix
+        continue #skip to next song   #umlautbugfix
+
+    ##  UMLAUTBUGFIX ENDE
 
     search_request_param = {"access_token": access_token,
                             "q": row[0] + " " + row[1],
@@ -74,7 +101,7 @@ for rownum, row in enumerate(csv_in):
         print("search_request_param: ")
         print(search_request_param)
 
-    if len(search_data["tracks"]["items"]) == 0:
+    if len(search_data["tracks"]["items"]) == 0: #song not found at spotify
         not_found_counter += 1
         if delete_not_found:
             try:
@@ -85,7 +112,7 @@ for rownum, row in enumerate(csv_in):
             csv_out.writerow((row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7],
                               row[8], row[9], row[10], row[11], row[12], row[13], row[14],
                               0, 0, 0, 0, 0, 0, 0, 0, 0,0,0))
-    else:
+    else: #song found at spotify
         track_id = search_data["tracks"]["items"][0]["id"]
         feature_request_param = {"access_token": access_token}
 
